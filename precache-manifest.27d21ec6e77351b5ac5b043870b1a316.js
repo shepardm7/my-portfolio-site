0739105c9865ae5336a3ba134a1f7d9824f15cbb
Sateek Roy <sateeksuper@gmail.com>
2020-01-02 19:53:10 -0500

    Updates

---
+++ precache-manifest.27d21ec6e77351b5ac5b043870b1a316.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb627c6d6276203d480beeda60684dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "938e8449f60216e0c656",
+    "url": "/static/css/2.ddc15c32.chunk.css"
+  },
+  {
+    "revision": "f3391e0e898562dec224",
+    "url": "/static/css/main.b181dc15.chunk.css"
+  },
+  {
+    "revision": "938e8449f60216e0c656",
+    "url": "/static/js/2.9d1b0ffe.chunk.js"
+  },
+  {
+    "revision": "f3391e0e898562dec224",
+    "url": "/static/js/main.65aeb352.chunk.js"
+  },
+  {
+    "revision": "31dbfd13e7003142faf7",
+    "url": "/static/js/runtime-main.a18719a9.js"
+  },
+  {
+    "revision": "34637669040230f3682710158f4ca2be",
+    "url": "/static/media/android.34637669.png"
+  },
+  {
+    "revision": "8187b4d00b6a49cc0da20c3b0e122291",
+    "url": "/static/media/blast-img.8187b4d0.png"
+  },
+  {
+    "revision": "85da782b2f58100182207a4b014f74d5",
+    "url": "/static/media/bulma.85da782b.png"
+  },
+  {
+    "revision": "0cb81e0e82633523b2e8e270c0f7ba04",
+    "url": "/static/media/crwn-img.0cb81e0e.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "f4914b528f07e408bac14225e21c4dc3",
+    "url": "/static/media/flask.f4914b52.png"
+  },
+  {
+    "revision": "97c3e4713e3008f76215cdd3050d57dc",
+    "url": "/static/media/gradle.97c3e471.jpg"
+  },
+  {
+    "revision": "5df16f2568585e1688ce2c7af32395ab",
+    "url": "/static/media/html-css-js.5df16f25.png"
+  },
+  {
+    "revision": "c37756252c6cdf9a97b0d2aa7011e579",
+    "url": "/static/media/java.c3775625.png"
+  },
+  {
+    "revision": "b3c5133feb48eaaf72ed89f1565a622a",
+    "url": "/static/media/jquery.b3c5133f.png"
+  },
+  {
+    "revision": "7522b8277ff3e692cf96f549a841d4ae",
+    "url": "/static/media/kotlin.7522b827.png"
+  },
+  {
+    "revision": "a9c62cf943c457fc610c551e4e209d7a",
+    "url": "/static/media/maven.a9c62cf9.png"
+  },
+  {
+    "revision": "0ba00c086d0e7bfce8f3c83d5b99bf58",
+    "url": "/static/media/me.0ba00c08.jpg"
+  },
+  {
+    "revision": "c00129be5a29d1bf10051657df17b23c",
+    "url": "/static/media/mongodb.c00129be.png"
+  },
+  {
+    "revision": "434d1b69daaf118b936e357572434586",
+    "url": "/static/media/mysql.434d1b69.png"
+  },
+  {
+    "revision": "b976114745d25bd41762676eef9d7eea",
+    "url": "/static/media/node.b9761147.jpg"
+  },
+  {
+    "revision": "649425185eab2b7c63d616b5d0374e4b",
+    "url": "/static/media/photoshop.64942518.png"
+  },
+  {
+    "revision": "b187e3b1985b0aa738093d97ce028ab6",
+    "url": "/static/media/php.b187e3b1.png"
+  },
+  {
+    "revision": "94e41097f90488c495c32403e8da8d30",
+    "url": "/static/media/pixabay-img.94e41097.png"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/static/media/react.04557c1b.jpg"
+  },
+  {
+    "revision": "fa45de2828f22cd232b1c7a1d1ffdfc0",
+    "url": "/static/media/sass.fa45de28.png"
+  },
+  {
+    "revision": "1556980f324f4f989cbf2208ddcbf19d",
+    "url": "/static/media/spring.1556980f.png"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/static/media/webpack.c500a380.png"
+  }
+]);
--- src/ActionTypes.js
+++
@@ -1,7 +0,0 @@
-export const actionType = {
-	onDocumentKeyUp: 'onDocumentKeyUp',
-	setShowSkip: 'setShowSkip',
-	setOnSkip: 'setOnSkip',
-	resetState: 'resetState',
-	setFormHasFocus: 'setFormHasFocus'
-};
